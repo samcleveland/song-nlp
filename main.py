fe884c4a8c1803174c1f0affa2bbb73fd4c3e440
samcleveland <52433135+samcleveland@users.noreply.github.com>
2024-10-17 13:47:25 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from spotify_data import Spotify_Data
+from dotenv import load_dotenv
+
+def main():
+    sd = Spotify_Data()
+
+
+if __name__ == "__main__":
+    main()
---
+++ spotify_data.py
@@ -0,0 +1,9 @@
+
+
+class Spotify_Data():
+    '''
+    Class to load in spotify song data  
+    
+    '''
+    def __init__(self):
+        print('Hello World!')
