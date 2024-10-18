79934a0ac2ee00d8955f59e46a525190b2a0be2a
samcleveland <52433135+samcleveland@users.noreply.github.com>
2024-10-17 20:59:21 -0500

    added artist and album download

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from spotify_data import Spotify_Data
 from dotenv import load_dotenv
+import pandas as pd
 
 def main():
     sd = Spotify_Data()
+    print(pd.DataFrame(sd.get_tracks('wetleg')['tracks']))
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
+
+sd = Spotify_Data()
+df = sd.get_artist_id('Big Thief')
+new_df = sd.get_albums(df.loc[0,'id'])
--- spotify_data.py
+++ spotify_data.py
@@ -1,4 +1,13 @@
+from dotenv import load_dotenv
+import os
 
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+import pandas as pd
+
+
+load_dotenv()
 
 class Spotify_Data():
     '''
@@ -6,4 +15,29 @@ class Spotify_Data():
     
     '''
     def __init__(self):
-        print('Hello World!')
+        CLIENT_ID: Final[str] = os.getenv('CLIENT_ID')
+        CLIENT_SECRET: Final[str] = os.getenv('CLIENT_SECRET')
+        
+        self.sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                           client_secret=CLIENT_SECRET))
+        
+        
+    def get_artist_id(self, artist: str, start_idx:int = 0) -> pd.DataFrame:
+        '''Returns the dataframe with artist ID, popularity, and genre for input artist name'''       
+        # Return top search results for artist
+        search_df: pd.DataFrame = pd.DataFrame(self.sp.search(artist, offset=start_idx, type='artist'))
+        
+        # Filter dataframe down to only artist info
+        artist_df: pd.DataFrame = pd.DataFrame(search_df.loc['items',:][0])
+        artist_df['name'] = artist_df['name'].apply(lambda row: row.lower())
+        
+        if len(artist_df[artist_df.name == artist.lower()]) == 1:
+            return artist_df[artist_df.name == artist.lower()]
+        elif start_idx >= 90:
+            return None
+        else:
+            return self.get_artist_id(artist, start_idx = start_idx + 10)
+        
+    def get_albums(self, artist_id:str = None) -> pd.DataFrame:
+        ''' Returns something '''
+        return self.sp.artist_albums(artist_id)
