0f77637d8e8eb71a5a427b05f0d0f9a6b14f5e30
samcleveland <52433135+samcleveland@users.noreply.github.com>
2024-10-21 20:57:53 -0500

    added comments to scrapper

--- artist_catalogue.py
+++ artist_catalogue.py
@@ -2,8 +2,17 @@ from spotify_data import Spotify_Data
 from dotenv import load_dotenv
 import pandas as pd
 
+from lyrics_scrapper import Lyric_Scrapper
+
 full_artist_list = ['Big Thief',
                     'Wet Leg',
+                    'Chappell Roan',
+                    'Charli xcx',
+                    'Frankie Cosmos',
+                    'Julia Jacklin',
+                    'Whitney',
+                    'Alvvays',
+                    'Snail Mail',
                     ]
 
 def main():
@@ -18,3 +27,7 @@ def main():
 sd = Spotify_Data()
 
 df_full = pd.concat([sd.build_artist_dataset(artist) for artist in full_artist_list])
+
+for idx, row in df_full.iterrows():
+    song_lyrics = Lyric_Scrapper(row['artist_name'], row['track_name'])
+    song_lyrics.get_lyrics()
